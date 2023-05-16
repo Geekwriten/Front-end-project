dcd9db9e3b2f1e37b6a1ade942e18762bb20c84c
geekwriter <rougiernoa12@gmail.com>
2023-05-16 20:55:58 +0200

    Deployed site

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,7 +2,7 @@
 const menuBtn = document.querySelector(".menu-button");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
-const menuBranding = document.querySelector(".menu-branding");
+// const menuBranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll('.nav-item');
 const menuNavsub = document.querySelectorAll('.menu-nav-sub');
 const Navsub = document.querySelectorAll('.nav-sub');
@@ -17,7 +17,7 @@ function toggleMenu() {
         menuBtn.classList.add('close');
         menu.classList.add('show');
         menuNav.classList.add('show');
-        menuBranding.classList.add('show');
+        // menuBranding.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
 
         // Set menu state
@@ -26,7 +26,7 @@ function toggleMenu() {
         menuBtn.classList.remove('close');
         menu.classList.remove('show');
         menuNav.classList.remove('show');
-        menuBranding.classList.remove('show');
+        // menuBranding.classList.remove('show');
         navItems.forEach(item => item.classList.remove('show'));
         
         // Set menu state
